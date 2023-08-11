704ae8c67a8c38dc5cb53ef5fed5ae3ceedccf17
Roman <jrj80mail@gmail.com>
2023-08-11 15:35:03 +0300

    add lessons

---
+++ Module_10/js/lesson_1.js
@@ -0,0 +1,12 @@
+// ********* Links ********* \\
+
+// https://media.licdn.com/dms/image/C5112AQEr5Pvwftcnpw/article-cover_image-shrink_600_2000/0/1538241940135?e=1687392000&v=beta&t=nBUq1TFjTuJmww0wqfhnd8A8SI_Vnq4S0F-xk2GI3YM
+// https://uk.wikipedia.org/wiki/SOAP
+// https://swapi.dev/
+
+// https://www.postman.com/downloads/
+// https://rickandmortyapi.com/documentation
+// https://www.weatherapi.com/docs/
+
+//* Приклад документації до API
+// https://rickandmortyapi.com/documentation/#rest
