d2820eeab14ac188c6c1524cc86b1fa8086091cc
Roman <jrj80mail@gmail.com>
2023-08-12 16:26:27 +0300

    Update lesson_1.js

--- Module_10/js/lesson_1.js
+++ Module_10/js/lesson_1.js
@@ -8,5 +8,29 @@
 // https://rickandmortyapi.com/documentation
 // https://www.weatherapi.com/docs/
 
+// https://developer.themoviedb.org/reference/trending-movies
+
 //* Приклад документації до API
 // https://rickandmortyapi.com/documentation/#rest
+
+//! -------- РИСІЧ 19-й урок --------
+
+//? 19:00
+// https://api.privatbank.ua/#p24/exchange
+
+//? 29:00
+//   postman.com
+
+//? 30:00
+// https://swapi.dev/documentation
+//? 32:00 (пояснення GPT)
+//* У JS термін "квері-параметри" використовується для посилання на частину URL, яка містить дані запиту,
+//* передані з одного веб-ресурсу на інший. Це може бути частина URL-адреси після символу запитання(?), включаючи параметри та їх значення,
+//* які передаються у вигляді пар "ключ=значення". Ці параметри використовуються для передачі інформації між різними сторінками, скриптами або серверами.
+
+//* Поточна URL-адреса: https://www.example.com/search?query=javascript&page=1
+const urlParams = new URLSearchParams(window.location.search);
+const query = urlParams.get("query"); // Отримати значення параметра "query"
+const page = urlParams.get("page"); // Отримати значення параметра "page" (ще один квері-параметр)
+console.log(query); // Виведе: "javascript"
+console.log(page); // Виведе: "1"
